3c2700e90c251f3aad3052dd44cfe65fe43e5a42
Berk Erdoğan <erdgnberk@gmail.com>
2023-11-28 00:02:46 +0300

    Splash and Logo
    Slplash and Logo screens were done

--- app/src/main/java/com/berkerdgn/tvseriesapplevel3/presentation/splash_screen_view/SecondSplashFragment.kt
+++ app/src/main/java/com/berkerdgn/tvseriesapplevel3/presentation/splash_screen_view/SecondSplashFragment.kt
@@ -1,14 +1,20 @@
 package com.berkerdgn.tvseriesapplevel3.presentation.splash_screen_view
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.berkerdgn.tvseriesapplevel3.R
+import com.berkerdgn.tvseriesapplevel3.databinding.FragmentSecondSplashBinding
 
 
 class SecondSplashFragment : Fragment() {
+
+    private var _binding : FragmentSecondSplashBinding ?=null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +26,25 @@ class SecondSplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second_splash, container, false)
+        _binding = FragmentSecondSplashBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.finishButton.setOnClickListener {
+            findNavController().navigate(R.id.action_onBoardingFragment_to_loginFragment)
+            onBoardingIsFinish()
+        }
+
+    }
+
+    private fun onBoardingIsFinish(){
+        val sharedPreferences = requireActivity().getSharedPreferences("onBoarding",Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("finished",true)
+        editor.apply()
+    }
 
 }
