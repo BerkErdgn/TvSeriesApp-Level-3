71e79bf9870639930fcb4d44de8327d9d81ad50a
Berk Erdoğan <erdgnberk@gmail.com>
2023-12-01 23:06:28 +0300

    except OneTvSeries UI
    All of them have been done except OneTvSeries UI.

--- app/src/main/java/com/berkerdgn/tvseriesapplevel3/data/repository/TvSeriesRepository.kt
+++ app/src/main/java/com/berkerdgn/tvseriesapplevel3/data/repository/TvSeriesRepository.kt
@@ -3,6 +3,7 @@ package com.berkerdgn.tvseriesapplevel3.data.repository
 import com.berkerdgn.tvseriesapplevel3.data.remote.model.allTvModels.AllTvSeriesModels
 import com.berkerdgn.tvseriesapplevel3.data.remote.model.searchModels.SearchTvSeriesModels
 import com.berkerdgn.tvseriesapplevel3.data.remote.model.todayModels.TodaysTvSeriesModels
+import com.berkerdgn.tvseriesapplevel3.data.remote.model.tvSeriesModels.TvSeriesModels
 import com.berkerdgn.tvseriesapplevel3.util.Resource
 
 interface TvSeriesRepository {
@@ -13,5 +14,7 @@ interface TvSeriesRepository {
     suspend fun getTodayTvSeries(data: String): Resource<TodaysTvSeriesModels>
 
     suspend fun getSearchTvSeries(q: String): Resource<SearchTvSeriesModels>
+
+    suspend fun getOneTvSeries(idTvSeries: String): Resource<TvSeriesModels>
 
 }
